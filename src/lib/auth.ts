f790016dfac957242bc77cc8ee511fdfbc82dae1
JotaPeans <jpfontesferreira1@gmail.com>
2025-08-09 16:39:21 -0300

    auth url

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
 import { createAuthClient } from "better-auth/react";
 export const { signIn, signUp, signOut, useSession, getSession } = createAuthClient({
-  baseURL: import.meta.env.VITE_AUTH_URL
+  baseURL: "https://day-messages-api-production.up.railway.app"
 });
