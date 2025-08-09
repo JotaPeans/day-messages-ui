65deb4aa05b09c609fb2046f630b3cd65dbeaafd
João Pedro <63313892+JotaPeans@users.noreply.github.com>
2025-08-09 17:10:14 -0300

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/sonner";
 
 export const Route = createRootRoute({
   component: () => (
-    <main className="min-h-screen w-full flex font-poppins">
+    <main className="min-h-full w-full flex font-poppins">
       <Toaster richColors position="top-right"/>
       <Outlet />
     </main>
