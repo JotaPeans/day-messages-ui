06a12e3e4efba6ffd812d82125366ef27cc1349a
João Pedro <63313892+JotaPeans@users.noreply.github.com>
2025-08-08 23:20:45 -0300

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
 import { createAuthClient } from "better-auth/react";
 export const { signIn, signUp, signOut, useSession, getSession } = createAuthClient({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_AUTH_URL
 });
